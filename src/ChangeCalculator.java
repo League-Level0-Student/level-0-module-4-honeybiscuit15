7d5106e646532f86b1963a56d89e332243a5fafb
honeybiscuit15 <league@WorthyMini-3.local>
2018-06-23 11:26:20 -0700

    6/23 change calculator

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -12,14 +12,21 @@ import javax.swing.JOptionPane;
 public class ChangeCalculator {
 
 	public static void main(String[] args) {
-
+String nickels =
 		// Ask the user how many nickels they have
-
+JOptionPane.showInternalInputDialog(null, "how many nickles do you have");
 		// Convert their answer to an int using Integer.parseInt()
-
+int answer= Integer.parseInt(nickels);
+	
 		// Ask the user how many dimes they have, and convert their answer
+String dimes=
+JOptionPane.showInternalInputDialog(null, "how many dimes do you have");
+int amount= Integer.parseInt(dimes);
 
 		// Ask the user how many quarters they have, and convert their answer
+String quarters=
+JOptionPane.showInternalInputDialog(null, "how many quarters do you have");
+int total= Integer.parseInt(quarters);
 
 		// Calculate how much money the user has and save it in a double variable 
 
